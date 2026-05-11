06bdc76259510344e411791f7d2a40030f357269
josecamilomunozr <munozroj@sheridancollege.ca>
2026-05-11 10:31:41 -0400

    student project master copy

---
+++ src/studentlist/Student.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author pepecamilo
+ */
+public class Student {
+    private String name; //encapsulation, todas las variables deben ser privadas
+    private int sid;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the sid
+     */
+    public int getSid() {
+        return sid;
+    }
+
+    /**
+     * @param sid the sid to set
+     */
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+    
+    
+}
