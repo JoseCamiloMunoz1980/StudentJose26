2964d50ef1156774ee3a99468bb15bea4774a121
josecamilomunozr <munozroj@sheridancollege.ca>
2026-05-25 10:17:54 -0400

    branch parttime

---
+++ src/studentlist/parttime.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author pepecamilo
+ */
+public class parttime {
+    
+    private int semester;
+
+    /**
+     * @return the semester
+     */
+    public int getSemester() {
+        return semester;
+    }
+
+    /**
+     * @param semester the semester to set
+     */
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+    
+}
